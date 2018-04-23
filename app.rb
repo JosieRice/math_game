6414d315e3fdfe75bdd83fc7e2868688522727f8
Craig Rice <craigricetong@gmail.com>
2018-04-23 23:52:28 +0000

    objects laid out for game

---
+++ app.rb
@@ -0,0 +1,19 @@
+##### Nouns / Entities
+
+# Player
+# Turns
+# Questions (Math Problems)
+# Numbers
+# Lives (3)
+# Scores
+
+
+
+#Player
+    #Lives (state)
+    #Scores (state)
+
+#Game
+    #Numbers (state)
+    #Question (behaviour)
+    #Turn (behaviour)
