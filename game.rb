8021cad6ad9dd994ee4356f74277cc341e72eedc
Craig Rice <craigricetong@gmail.com>
2018-04-24 05:50:20 +0000

    small refactoring, started to work on new math problems to add

--- game.rb
+++ game.rb
@@ -13,9 +13,14 @@ class Game
 
     # play sequence, player 1 turn, then player 2 turn, then a check if the game is over
     def play
+        puts "-----NEW TURN-----"
         addition @player1, @player2
         addition @player2, @player1
-        end_game
+        end_game_check
+    end
+
+    def stage2
+        puts "I'm in stage 2"
     end
 
     # one turn for one player, params put in reverse for opponents turn
@@ -23,25 +28,28 @@ class Game
         @logic.question
         puts "#{player1.name}: What does #{@logic.num1} plus #{@logic.num2} equal?"
         print "> "
-        answer = $stdin.gets.chomp     
+        answer = $stdin.gets.chomp   
         if answer == "#{@logic.answer}"
-            puts "That is correct"
+            puts "#{answer} is correct"
             player1.score += 1
             puts "#{player1.name}: #{player1.score} correct"
         else
-            puts "That's incorrect"
+            puts "#{answer} is incorrect"
             player1.lives -= 1
             puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
         end     
     end
 
     # The check to see if one of the players is at 0 lives, if not, take another turn
-    def end_game
+    def end_game_check
         if @player1.lives == 0 && @player2.lives == 0
-            puts "It's a tie, you both need to work on your math skillz"
+            puts "-----GAME OVER-----"
+            puts "It's a tie, you both need to work on your math skills"
         elsif @player1.lives == 0
+            puts "-----GAME OVER-----"
             puts "Player 1 loses."
         elsif @player2.lives == 0
+            puts "-----GAME OVER-----"
             puts "Player 2 loses."
         else
            play
--- logic.rb
+++ logic.rb
@@ -8,10 +8,17 @@ class Logic
         @answer = 0
     end
 
-    # get and set numbers for math question
+    # get and set numbers for addition math question
     def question
         self.num1 = [*1..20].sample
         self.num2 = [*1..20].sample
         self.answer = num1 + num2
+    end
+
+    # get and set numbers for multiplication question
+    def multiplication
+        self.num1 = [*1..10].sample
+        self.num2 = [*1..10].sample
+        self.answer = num1 * num2
     end
 end
